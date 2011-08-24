67c2322bc8bdaae0181704e01e0a3ae541b771d6
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-08-23 21:53:03 -0400

    First test, and fix makefile.

---
+++ example-app/test/app.test.js
@@ -0,0 +1,13 @@
+var app = require('../app'),
+    assert = require('assert');
+
+module.exports = { 
+  'Load the home page': function() { 
+    assert.response(app, 
+        { url: '/' }, 
+        { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' }},
+        function(response) { 
+          assert_includes(response.body, 'Welcome');
+        });
+  }
+};
